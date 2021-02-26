217fab156a412467654ad6a249170a97cc9ded8b
mgrayhack <mgrayhack@chicagobooth.edu>
2021-02-26 02:23:49 +0000

    first index done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-
-
+ get("/users", { :controller => "users", :action => "index" })
+ get("/", { :controller => "users", :action => "index" })
+get("/users/:username_path", { :controller => "users", :action => "show" })
 end
